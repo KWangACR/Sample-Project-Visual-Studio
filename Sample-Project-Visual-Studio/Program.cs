c4bb66cbad41dc68661dc9370245ed8aef3f3d16
kwang <kwang@acr.org>
2019-12-05 20:18:24 -0500

    Modify console output in "feature/test-branch-out" branch.

--- Sample-Project-Visual-Studio/Program.cs
+++ Sample-Project-Visual-Studio/Program.cs
@@ -6,7 +6,7 @@ namespace Sample_Project_Visual_Studio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!  This is from feature/test-branch-out branch");
         }
     }
 }
