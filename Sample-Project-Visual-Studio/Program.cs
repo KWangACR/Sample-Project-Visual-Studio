d3e9b7289b72bf94a03c6213dd3eeb53c137d61e
kwang <kwang@acr.org>
2019-12-05 19:11:49 -0500

    Created a console app (.NET Core) project.

---
+++ Sample-Project-Visual-Studio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample_Project_Visual_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
